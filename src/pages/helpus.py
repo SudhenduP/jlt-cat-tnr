8731fe102e6b22fa634552223f62d46854a681ed
SudhenduP <pandeysudhendu@gmail.com>
2020-09-03 12:41:19 +0400

    Update helpus.py

--- src/pages/helpus.py
+++ src/pages/helpus.py
@@ -1,20 +1,18 @@
 import streamlit as st
 
 
-
-
 def help_section():
     st.subheader("How can you help? Glad you asked 😻")
 
     st.markdown("""
     1. **Volunteer to feed cats around your area** – let Mel know if this is something you are interested in.  She will provide you with a feeding badge and add you to the feeding list. [Feeder List](https://api.whatsapp.com/send?phone=971551283234&text=Hi%2C%20I%20would%20like%20to%20help%20with%20Cat%20Feeding%20for%20JLT%20Cats!%20Let%20me%20know%20how%20%F0%9F%98%8A)
-    
+
     2. **Logging the cats in your area** – we need to try and keep our Cat-A-Log as up to date as possible and need everyone’s help with this. [Cat-a-log Whatsapp](https://api.whatsapp.com/send?phone=971551283234)
-    
-    3. **Fostering** – do you have room to foster one of our babies – or even adopting – details are on the website.  Every cat deserves a friend at home. [Foster](https://api.whatsapp.com/send?phone=971551283234)
-    
-    4. **Providing things for our JLT Flea Market** – we take a table every flea market to help raise funds towards our JLT Community Cat ongoing vet bills.  Can you help us out on the day [Flea Market](https://api.whatsapp.com/send?phone=971551283234)
-    
+
+    3. **Fostering** – do you have room to foster one of our babies – or even adopting – details are on the website.  Every cat deserves a friend at home. [Foster](https://api.whatsapp.com/send?phone=971551283234&text=Hi%20I%20would%20like%20to%20help%20with%20fostering!%20Let%20me%20know%20how%20%F0%9F%98%8A)
+
+    4. **Providing things for our JLT Flea Market** – we take a table every flea market to help raise funds towards our JLT Community Cat ongoing vet bills.  Can you help us out on the day [Flea Market](https://api.whatsapp.com/send?phone=971551283234&text=Hi,%20I%20have%20some%20stuff%20I%20would%20like%20to%20donate%20for%20JLT%20Flea%20Market%20%F0%9F%98%8A)
+
     5. **Going on holiday/business travel?**  Let us know, we visit and look after your cat when you are away.  A small charge of (AED 50 per day) of which the whole amount will be paid towards our JLT Community Cat ongoing vet bills. [Cat Sitting](https://api.whatsapp.com/send?phone=971551283234)
 """)
 
